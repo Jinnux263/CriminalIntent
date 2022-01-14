038a59f7393d5af3e69b3628d8ddeffd5606c984
Jinnux <nguyentandat26032001@gmail.com>
2022-01-14 16:30:11 +0700

    update

--- app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
@@ -4,11 +4,17 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
 
 
 class DatePickerFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val v: View = LayoutInflater.from(activity)
+            .inflate(R.layout.dialog_date, null)
+
         return AlertDialog.Builder(requireContext())
+            .setView(v)
             .setTitle(R.string.date_picker_title)
             .setPositiveButton(android.R.string.ok, null)
             .create()
